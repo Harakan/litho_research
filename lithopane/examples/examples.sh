2db89b3a590d2032bc0cc41b83bdfed2f7fa1ec7
Will Topping <will@nowhere.com>
2016-12-26 15:22:54 -0600

    Cool, that works for pngs. Now to test for settings

---
+++ lithopane/examples/examples.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+../lithophane.js -i reference.png
+../lithophane.js -i reference-as-lithophane.png -o reference_as_litho.stl
+
+#This kills it:
+#./lithophane.js -i ie1_ramjam.jpg -o ramo.stl
