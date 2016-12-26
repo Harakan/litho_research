46293ec25583251e8c60a992cc56b1062f3993f1
Will Topping <will@nowhere.com>
2016-12-26 15:04:27 -0600

    Setup and couple more examples. Testing time!

---
+++ lithopane/setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#What this does:
+#
+# 1) Gets any debian (linux) packages I was missing
+# 2) follows the good instructions at http://wubbahed.github.io/lithophane/
+
+sudo apt install nodejs-legacy npm
+
+npm install get-pixels
+npm install commander
